2ae5375d58ff4c25a5d4337c8f3e6671c65ecd87
Bookis Smuin <vegan.bookis@gmail.com>
2012-09-24 11:43:11 -0700

    remove puts

--- lib/responsive_picture/railtie.rb
+++ lib/responsive_picture/railtie.rb
@@ -2,7 +2,6 @@ require 'responsive_picture/view_helpers'
 module ResponsivePicture
   class Railtie < Rails::Railtie
     initializer "responsive_picture.view_helpers" do
-      puts "***&&&"
       ActionView::Base.send :include, ViewHelpers
     end
   end
