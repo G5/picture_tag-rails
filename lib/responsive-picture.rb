586e219619a2119c7d4465658526ddee4981e0cc
Bookis Smuin <vegan.bookis@gmail.com>
2012-09-24 11:42:41 -0700

    remove old file names

--- lib/responsive-picture.rb
+++
@@ -1,7 +0,0 @@
-require "responsive-picture/version"
-
-module Responsive
-  module Picture
-    # Your code goes here...
-  end
-end
--- lib/responsive-picture/version.rb
+++
@@ -1,5 +0,0 @@
-module Responsive
-  module Picture
-    VERSION = "0.0.1"
-  end
-end
