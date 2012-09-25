d201eb953434b04f25095100aecc4aecf88aa65b
Bookis Smuin <vegan.bookis@gmail.com>
2012-09-25 13:27:03 -0700

    slim down spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,32 +1,13 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-ENV["RAILS_ENV"] ||= 'test'
-
 require 'rubygems'
 
-# Only the parts of rails we want to use
-# if you want everything, use "rails/all"
 require "action_controller/railtie"
-require "rails/test_unit/railtie"
 
-root = File.expand_path(File.dirname(__FILE__))
-
-# Define the application and configuration
 module Config
   class Application < ::Rails::Application
-    config.assets.enabled = true
-    config.assets.version = '1.1'
     config.active_support.deprecation = :stderr 
   end
 end
-
-# Initialize the application
 Config::Application.initialize!
 
 require 'rspec/rails'
-
-RSpec.configure do |config|
-end
-
 require 'picture_tag'
