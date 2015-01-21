baa9e874077e619487e43b6afc6cfacf8ea4d216
Colton Montana Rice Fent <coltomatic@gmail.com>
2015-01-21 13:04:23 -0800

    configure and stuff

--- lib/picture_tag-rails.rb
+++ lib/picture_tag-rails.rb
@@ -1,7 +1,16 @@
 require "picture_tag-rails/version"
 require 'picture_tag-rails/railtie'
 
-def configure
-  self.configuration = Configuration.new
-  yield(configuration)
+require_relative "./picture_tag-rails/configuration"
+
+module PictureTag
+  class << self
+    attr_accessor :configuration
+
+      # Pattern inspired by http://robots.thoughtbot.com/mygem-configure-block/
+    def configure
+      self.configuration = Configuration.new
+      yield(configuration)
+    end
+  end
 end
---
+++ lib/picture_tag-rails/configuration.rb
@@ -0,0 +1,12 @@
+module PictureTag
+  class Configuration
+
+    attr_accessor :display_high_def
+
+    def initialize
+      @display_high_def = true
+    end
+
+  end
+
+end
